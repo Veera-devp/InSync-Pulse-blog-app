e12d942dd562166dfb26f7d920b8668dcb530b16
Veera-devp <saikam.raghava@gmail.com>
2024-01-05 21:01:49 +0530

    Created Post to the blog

--- src/main/java/com/realtime/blogapp/controller/PostController.java
+++ src/main/java/com/realtime/blogapp/controller/PostController.java
@@ -25,5 +25,12 @@ public class PostController {
             model.addAttribute("posts", posts);
             return "/admin/posts";
       }
+      @GetMapping("admin/posts/newpost")
+      public String newPostForm(Model model)
+      {
+            PostDto postDto = new PostDto();
+            model.addAttribute("post", postDto);
+            return "admin/create_post";
+      }
 
 }
