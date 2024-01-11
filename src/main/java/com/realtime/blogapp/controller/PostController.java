e8a6903937d48ea8df2cca2831f9481518b984cb
Veera-devp <saikam.raghava@gmail.com>
2024-01-11 23:13:57 +0530

    Blog search Admin

--- src/main/java/com/realtime/blogapp/controller/PostController.java
+++ src/main/java/com/realtime/blogapp/controller/PostController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.realtime.blogapp.dto.PostDto;
 import com.realtime.blogapp.service.PostService;
@@ -98,5 +99,13 @@ public class PostController {
             String url = title.replaceAll("\\s+", "-");
             url = url.replaceAll("[^A-Za-z0-9]", "-");
             return url;
+      }
+
+      @GetMapping("/admin/posts/search")
+      public String searchPosts(@RequestParam(value = "query") String query,
+                  Model model) {
+            List<PostDto> posts = postService.searchPosts(query);
+            model.addAttribute("posts", posts);
+            return "admin/posts";
       }
 }
--- src/main/java/com/realtime/blogapp/repository/PostRepository.java
+++ src/main/java/com/realtime/blogapp/repository/PostRepository.java
@@ -1,11 +1,17 @@
 package com.realtime.blogapp.repository;
 
-import java.util.Optional;
+import java.util.*;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.realtime.blogapp.entity.Post;
 
-public interface PostRepository extends JpaRepository<Post,Long>{
+public interface PostRepository extends JpaRepository<Post, Long> {
       Optional<Post> findByUrl(String url);
+
+      @Query("SELECT p from Post p WHERE " +
+                  " p.title LIKE CONCAT('%', :query, '%') OR " +
+                  " p.shortDescription LIKE CONCAT('%', :query, '%')")
+      List<Post> searchPosts(String query);
 }
