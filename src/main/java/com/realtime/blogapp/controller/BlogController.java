e4699b80f3fc1f19d9ef2c5b0cd94b828d39a1dd
Veera-devp <saikam.raghava@gmail.com>
2024-01-12 20:30:11 +0530

    User Blog

---
+++ src/main/java/com/realtime/blogapp/controller/BlogController.java
@@ -0,0 +1,43 @@
+package com.realtime.blogapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.realtime.blogapp.dto.PostDto;
+import com.realtime.blogapp.service.PostService;
+import java.util.*;
+
+@Controller
+public class BlogController {
+      private PostService postService;
+
+      public BlogController(PostService postService) {
+            this.postService = postService;
+      }
+
+      @GetMapping("/")
+      public String viewBlogPosts(Model model) {
+            List<PostDto> postResponse = postService.findallPosts();
+            model.addAttribute("postsResponse", postResponse);
+            return "blog/view_posts";
+      }
+
+      @GetMapping("/post/{postUrl}")
+      private String showPost(@PathVariable("postUrl") String postUrl,
+                  Model model) {
+            PostDto post = postService.findPostByUrl(postUrl);
+            model.addAttribute("post", post);
+            return "blog/blog_post";
+      }
+      @GetMapping("/page/search")
+      public String searchPosts(@RequestParam(value = "query") String query,
+                  Model model) {
+            List<PostDto> postsResponse = postService.searchPosts(query);
+            model.addAttribute("postsResponse", postsResponse);
+            return "blog/view_posts";
+      }
+
+}
